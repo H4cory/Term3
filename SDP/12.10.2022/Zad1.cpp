281cb2c3d7edad5169289f9ef075dda3c98ca104
H4cory <k.kanev13@gmail.com>
2022-11-04 13:56:09 +0200

    a

--- SDP/12.10.2022/Zad1.cpp
+++ SDP/12.10.2022/Zad1.cpp
@@ -7,61 +7,75 @@
 
 using namespace std;
 
-
-int main(){
+int main()
+{
    string input;
    stack<char> bira[1];
+   char output[50];
+   int cOutput = 0;
 
-while(true){
+   while (true)
+   {
 
-   cout<<"Input: ";
-   getline(cin,input);
+      cout << "Input: ";
+      getline(cin, input);
 
-   for(int i =0;i<input.size();i++){
-    
+      for (int i = 0; i < input.size(); i++)
+      {
 
-      if((123 > input[i]&& input[i]>96)||(91 > input[i]&& input[i]>64)){
-         //look for smaller case chars
-         cout<<input[i];
-      }
-      else if(input[i]=='/'||input[i]=='*'||input[i]=='('){
-         bira->push(input[i]);
-         
-      }
-      else if(input[i]=='+'||input[i]=='-'){
-         if(bira->top()=='*'||bira->top()=='/'){
-             while(!bira->empty()){
-            cout<<bira->top();
-            bira->pop();
-   }
-   bira->push(input[i]);
+         if ((123 > input[i] && input[i] > 96) || (91 > input[i] && input[i] > 64))
+         {
+
+            // cout<<input[i];
+            output[cOutput++] = input[i];
          }
-         else{
+         else if (input[i] == '/' || input[i] == '*' || input[i] == '(')
+         {
             bira->push(input[i]);
          }
-      }
-      else if(input[i]==')'){
-         while(true){
-            if(bira->top()=='('){
-               bira->pop();
-               break;
+         else if (input[i] == '+' || input[i] == '-')
+         {
+            if (bira->top() == '*' || bira->top() == '/')
+            {
+               while (!bira->empty())
+               {
+                  // cout<<bira->top();
+                  output[cOutput++] = bira->top();
+                  bira->pop();
+               }
+               bira->push(input[i]);
             }
             else
-            cout<<bira->top();
-            bira->pop();
+            {
+               bira->push(input[i]);
+            }
          }
-
+         else if (input[i] == ')')
+         {
+            while (true)
+            {
+               if (bira->top() == '(')
+               {
+                  bira->pop();
+                  break;
+               }
+               else
+                  // cout<<bira->top();
+                  output[cOutput++] = bira->top();
+               bira->pop();
+            }
+         }
       }
-   }
 
-   while(!bira->empty()){
-      cout<<bira->top();
-            bira->pop();
-   }
-   cout<<endl;
-   
+      while (!bira->empty())
+      {
+         // cout<<bira->top();
+         output[cOutput++] = bira->top();
+         bira->pop();
+      }
+      cout << output;
+      cout << endl;
    }
    return 0;
 }
-// a+b+(c+d)
-// ab
+// c*((a+b)*(a-b))*c-a
