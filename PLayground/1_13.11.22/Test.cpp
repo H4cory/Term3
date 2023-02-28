1f9e1367a7287fed7dcd072e098e984e1e78fc85
H4coryLX <k.kanev13@gmail.com>
2023-02-28 14:11:36 +0200

    ad

--- PLayground/1_13.11.22/Test.cpp
+++ PLayground/1_13.11.22/Test.cpp
@@ -1,7 +1,7 @@
 /*
 --Specifikaciq
-    Celta na tazi programa e da se sortirat chisla w masif i sled towa po opredeln nachin da 
-se nameri tyrseni chislo.
+    Celta na tazi programa e da se sortirat chisla w masif i sled towa po opredeln nachin da
+se nameri tyrseno chislo.
     Kato nachalo chisla trqbwa da mogat da bydat wywejdani ot potrebitelq, opciq za
 proizwolni chisla.
 
@@ -23,53 +23,60 @@ Date :                            Last Modified:
 
 using namespace std;
 
-
-
-
-
-int main(){
-
-    vector<int> input;
+void getData(vector<float> &input)
+{
     string tempInput;
     int i = 0;
 
-    cout<<"\nInsert s to stop!\nPlease insert numbers: \n";
+    cout << "\nInsert s to stop!\nPlease insert numbers: \n";
 
-    //Get user input
-    while(true)
-    {   
+    // Get user input
+    while (true)
+    {
 
-        cout<< i+1 <<" > ";
-        getline(cin,tempInput);
+        cout << i + 1 << " > ";
+        getline(cin, tempInput);
 
-        if(tempInput[0] == 's' || tempInput[0] == 'S'){
+        if (tempInput[0] == 's' || tempInput[0] == 'S')
+        {
             break;
         }
 
-
         try
-         {
+        {
             input.push_back(stoi(tempInput));
-         }
-         catch (exception &e)
-         {
+        }
+        catch (exception &e)
+        {
             cout << "> Error converting" << '\n';
             i--;
-         }
-         i++;
-        
-    }
-
-
-    //Display user input
-    i = 0;
-    cout<<"\nOutput = { ";
-    while( i < input.size()){
-        cout<<input[i]<<' ';
+        }
         i++;
     }
-    cout<<"} \n";
-    
-   
-   return 0;
+}
+
+void displayData(vector<float> &input)
+{
+
+    int i = 0;
+
+    // Display user input
+    cout << "\nOutput = { ";
+    while (i < input.size())
+    {
+        cout << input[i] << ' ';
+        i++;
+    }
+    cout << "} \n";
+}
+
+int main()
+{
+
+    vector<float> input;
+
+    getData(input);
+    displayData(input);
+
+    return 0;
 }
